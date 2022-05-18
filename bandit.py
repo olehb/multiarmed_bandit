86ddbddee8443609fb48a5d74c213eaa1b31bb7e
Oleg Boiko <oboiko@openai.com>
2022-05-18 10:00:35 -0700

    better estimates

--- bandit.py
+++ bandit.py
@@ -1,6 +1,7 @@
 import random
 import numpy as np
 import abc
+from typing import Callable
 
 from multiprocessing import Pool
 
@@ -12,8 +13,9 @@ class World:
         self._deviations = np.ones(num_actions)
         self._optimal_action = np.argmax(self._means)
 
-    def take_action(self, action_id: int) -> float:
-        return np.random.normal(self._means[action_id], self._deviations[action_id])
+    def take_action(self, action_id: int) -> tuple[float, int]:
+        is_optimal = int(action_id == self._optimal_action)
+        return np.random.normal(self._means[action_id], self._deviations[action_id]), is_optimal
 
     @property
     def id(self):
@@ -24,21 +26,22 @@ class World:
 
 
 class Agent(abc.ABC):
-    def __init__(self, num_actions: int, steps_per_epoch: int):
+    def __init__(self, num_actions: int):
         self._num_actions = num_actions
-        self._rewards = np.zeros((num_actions, steps_per_epoch))
-        self._steps_elapsed = 0
+        self._reward_estimates = np.zeros(num_actions)
+        self._total_reward = 0
 
     def receive_reward(self, action_id: int, reward: float) -> None:
-        self._rewards[action_id][self._steps_elapsed] = reward
-        self._steps_elapsed += 1
+        self._reward_estimates[action_id] += 0.01*(reward - self._reward_estimates[action_id])
+        self._total_reward += reward
 
     @abc.abstractmethod
     def next_action(self) -> int:
         pass
 
+    @property
     def total_reward(self) -> int:
-        return int(np.sum(self._rewards))
+        return self._total_reward
 
     @property
     def name(self):
@@ -55,55 +58,63 @@ class RandomAgent(Agent):
 
 class GreedyAgent(Agent):
     def next_action(self) -> int:
-        return int(np.argmax(np.max(self._rewards, axis=1)))
+        return int(np.argmax(self._reward_estimates))
 
 
 class EGreedyAgent(Agent):
-    def __init__(self, num_actions: int, steps_per_epoch: int, eps: float = 0.01):
-        super().__init__(num_actions, steps_per_epoch)
+    def __init__(self, num_actions: int, eps: float = 0.01):
+        super().__init__(num_actions)
         self._eps = eps
 
     def next_action(self) -> int:
         if random.random() <= self._eps:
             return np.random.randint(0, self._num_actions)
         else:
-            return int(np.argmax(np.max(self._rewards, axis=1)))
+            return int(np.argmax(self._reward_estimates))
 
     @property
     def name(self):
         return f"{super().name}_{self._eps}"
 
 
-def run_world(world: World, agent: Agent, num_steps: int) -> tuple[World, Agent]:
-    for _ in range(num_steps):
+def run_experiment(agent: Agent, agent_id: int, world: World, world_id: int, steps_per_world: int):
+    result = np.zeros((steps_per_world, 4))
+    for s in range(steps_per_world):
         action_id = agent.next_action()
-        reward = world.take_action(action_id)
+        reward, is_optimal = world.take_action(action_id)
         agent.receive_reward(action_id, reward)
-    return world, agent
+
+        result[s][0] = agent_id
+        result[s][1] = world_id
+        result[s][2] = reward
+        result[s][3] = is_optimal
+    return result
 
 
 def main():
     num_actions = 10
-    num_worlds = 100
-    steps_per_epoch = 2000
-
-    worlds = [World(i, num_actions) for i in range(num_worlds)]
+    num_worlds = 10
+    steps_per_world = 20
 
     agent_creators = [
-        lambda: RandomAgent(num_actions, steps_per_epoch),
-        lambda: GreedyAgent(num_actions, steps_per_epoch),
-        lambda: EGreedyAgent(num_actions, steps_per_epoch, eps=0.01),
-        lambda: EGreedyAgent(num_actions, steps_per_epoch, eps=0.1),
+        lambda: RandomAgent(num_actions),
+        lambda: GreedyAgent(num_actions),
+        lambda: EGreedyAgent(num_actions, eps=0.01),
+        lambda: EGreedyAgent(num_actions, eps=0.1),
     ]
 
-    with Pool(6) as pool:
-        world_agent_pairs = [
-            (world, create_agent(), steps_per_epoch) for world in worlds for create_agent in agent_creators
-        ]
-        world_agent_pairs = pool.starmap(run_world, world_agent_pairs)
+    create_world = lambda i: World(i, num_actions)
 
-    for (world, agent) in enumerate(world_agent_pairs):
-        print(agent.name, agent.total_reward())
+    args = [
+        (create_agent(), agent_id, create_world(world_id), world_id, steps_per_world)
+        for world_id in range(num_worlds)
+        for agent_id, create_agent in enumerate(agent_creators)
+    ]
+    with Pool(len(agent_creators)) as pool:
+        results = pool.starmap(run_experiment, args)
+
+    aggregated_result = np.stack(results, axis=2)
+    print(aggregated_result.shape)
 
 
 if __name__ == '__main__':
