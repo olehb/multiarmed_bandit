e035cbffc371b1df8d01762fe10c65cccd48d3cc
Oleg Boiko <oboiko@openai.com>
2022-05-18 15:00:44 -0700

    parameterizing learning_rate

--- bandit.py
+++ bandit.py
@@ -1,9 +1,8 @@
-import random
-import numpy as np
 import abc
-from typing import Callable
-
+import random
 from multiprocessing import Pool
+
+import numpy as np
 
 
 class World:
@@ -15,38 +14,45 @@ class World:
 
     def take_action(self, action_id: int) -> tuple[float, int]:
         is_optimal = int(action_id == self._optimal_action)
-        return np.random.normal(self._means[action_id], self._deviations[action_id]), is_optimal
+        return (
+            np.random.normal(self._means[action_id], self._deviations[action_id]),
+            is_optimal,
+        )
 
     @property
     def id(self):
         return self._id
-    
+
     def __repr__(self):
         return f"world_{self.id}"
 
 
 class Agent(abc.ABC):
-    def __init__(self, num_actions: int):
+    def __init__(self, num_actions: int, initial_estimate: float = 0.0, learning_rate:float = 0.01):
         self._num_actions = num_actions
-        self._reward_estimates = np.zeros(num_actions)
-        self._total_reward = 0
+        self._reward_estimates = np.full(
+            (num_actions,), initial_estimate, dtype=np.float
+        )
+        self._initial_estimate = initial_estimate
+        self._learning_rate = learning_rate
 
     def receive_reward(self, action_id: int, reward: float) -> None:
-        self._reward_estimates[action_id] += 0.01*(reward - self._reward_estimates[action_id])
-        self._total_reward += reward
+        self._reward_estimates[action_id] += self._learning_rate * (
+            reward - self._reward_estimates[action_id]
+        )
 
     @abc.abstractmethod
     def next_action(self) -> int:
         pass
 
     @property
-    def total_reward(self) -> int:
-        return self._total_reward
-
-    @property
     def name(self):
-        return self.__class__.__name__
-    
+        class_name = self.__class__.__name__
+        if self._initial_estimate == 0:
+            return class_name
+        else:
+            return f"{class_name}_est{self._initial_estimate}"
+
     def __repr__(self):
         return self.name
 
@@ -62,8 +68,8 @@ class GreedyAgent(Agent):
 
 
 class EGreedyAgent(Agent):
-    def __init__(self, num_actions: int, eps: float = 0.01):
-        super().__init__(num_actions)
+    def __init__(self, num_actions: int, initial_estimate: float = 0.0, learning_rate: float = 0.01, eps: float = 0.01):
+        super().__init__(num_actions, initial_estimate, learning_rate)
         self._eps = eps
 
     def next_action(self) -> int:
@@ -77,7 +83,9 @@ class EGreedyAgent(Agent):
         return f"{super().name}_{self._eps}"
 
 
-def run_experiment(agent: Agent, agent_id: int, world: World, world_id: int, steps_per_world: int):
+def run_experiment(
+    agent: Agent, agent_id: int, world: World, world_id: int, steps_per_world: int
+):
     result = np.zeros((steps_per_world, 4))
     for s in range(steps_per_world):
         action_id = agent.next_action()
@@ -93,14 +101,14 @@ def run_experiment(agent: Agent, agent_id: int, world: World, world_id: int, ste
 
 def main():
     num_actions = 10
-    num_worlds = 10
-    steps_per_world = 20
+    num_worlds = 2
+    steps_per_world = 3
 
     agent_creators = [
         lambda: RandomAgent(num_actions),
         lambda: GreedyAgent(num_actions),
         lambda: EGreedyAgent(num_actions, eps=0.01),
-        lambda: EGreedyAgent(num_actions, eps=0.1),
+        # lambda: EGreedyAgent(num_actions, eps=0.1),
     ]
 
     create_world = lambda i: World(i, num_actions)
@@ -116,9 +124,10 @@ def main():
     aggregated_result = np.stack(results, axis=2)
     print(aggregated_result.shape)
 
+    a = np.sum(aggregated_result, axis=0)
+    aggregated_by_world = a.reshape((a.shape[0], num_worlds, len(agent_creators)))
+    print(aggregated_by_world.shape)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
-
-
-
